5faf2a3833bdd20b9b40cc50373a258c4d1ca3d3
Chwaksen <agustinmandarini47@gmail.com>
2023-05-11 20:32:18 -0300

    Commented style import in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Cards from "./components/Cards/Cards.jsx";
 import SearchBar from "./components/SearchBar/SearchBar.jsx";
 import characters from "./data.js";
-import style from "./App.module.css";
+// import style from "./App.module.css";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
